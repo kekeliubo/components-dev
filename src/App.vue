c5d64584419f3d5592199e8abda927610f524e7a
liubo <2998739650@qq.com>
2023-01-31 19:20:16 +0800

    初始化

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,66 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- type -->
+    <div>
+      <emui-Button type="default">取消</emui-Button>
+      <emui-Button type="primarywhite">确认</emui-Button>
+      <emui-Button type="primarygold">确认</emui-Button>
+      <emui-Button type="successwhite">测试</emui-Button>
+      <emui-Button type="successgold">测试</emui-Button>
+      <emui-Button type="warning">测试</emui-Button>
+      <emui-Button type="dangerwhite">测试</emui-Button>
+      <emui-Button type="dangergold" cut>测试</emui-Button>
+    </div>
+    <!-- round -->
+    <div>
+      <emui-Button type="default" round>取消</emui-Button>
+      <emui-Button type="primarywhite" round>确认</emui-Button>
+      <emui-Button type="primarygold" round>确认</emui-Button>
+      <emui-Button type="successwhite" round>测试</emui-Button>
+      <emui-Button type="successgold" round>测试</emui-Button>
+      <emui-Button type="warning" round>测试</emui-Button>
+      <emui-Button type="dangerwhite" round>测试</emui-Button>
+      <emui-Button type="dangergold" round>测试</emui-Button>
+    </div>
+    <!-- plain -->
+    <div>
+      <emui-Button type="default" plain>取消</emui-Button>
+      <emui-Button type="primarywhite" plain>确认</emui-Button>
+      <emui-Button type="primarygold" plain>确认</emui-Button>
+      <emui-Button type="successwhite" plain>测试</emui-Button>
+      <emui-Button type="successgold" plain>测试</emui-Button>
+      <emui-Button type="warning" plain>测试</emui-Button>
+      <emui-Button type="dangerwhite" plain>测试</emui-Button>
+      <emui-Button type="dangergold" plain>测试</emui-Button>
+    </div>
+    <div>
+      <emui-Button type="default" disabled>取消</emui-Button>
+      <emui-Button type="primarywhite" disabled>确认</emui-Button>
+      <emui-Button type="primarygold" disabled>确认</emui-Button>
+      <emui-Button type="successwhite" disabled>测试</emui-Button>
+      <emui-Button type="successgold" disabled>测试</emui-Button>
+      <emui-Button type="warning" disabled>测试</emui-Button>
+      <emui-Button type="dangerwhite" disabled>测试</emui-Button>
+      <emui-Button type="dangergold" disabled>测试</emui-Button>
+    </div>
+    <div>
+      <emui-Button type="default" circle>取消</emui-Button>
+      <emui-Button type="primarywhite" circle>确认</emui-Button>
+      <emui-Button type="primarygold" circle>确认</emui-Button>
+      <emui-Button type="successwhite" circle>测试</emui-Button>
+      <emui-Button type="successgold" circle>测试</emui-Button>
+      <emui-Button type="warning" circle>测试</emui-Button>
+      <emui-Button type="dangerwhite" circle>测试</emui-Button>
+      <emui-Button type="dangergold" circle>测试</emui-Button>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped lang="scss">
 </style>
